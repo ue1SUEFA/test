9747d560ab5b064b7bb59d7f21476f98085287bd
Afzal <karimovafzal22@gmail.com>
2024-04-15 18:58:04 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-drag-and-drop:options="options">
+    <h1 class="title">Todo</h1>
+    <ul>
+      <li v-for="group in groups " :key="group.id">
+        <span>
+          <h2 class="group_name">{{ group.name }}</h2>
+        </span>
+        <vue-draggable-group v-model="group.items" :groups="groups" :data-id="group.id" @change="onGroupsChange">
+          <ul class="drag-list" :data-id="id">
+            <li class="item-list" v-for="item in group.items" :key="item.id" :data-id="item.id">
+              <div>{{ item.title }}</div>
+            </li>
+          </ul>
+        </vue-draggable-group>
+
+      </li>
+    </ul>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      groups: [
+        {
+          id: 1,
+          name: "Todoes",
+
+          items: [
+            { id: 1, title: "todo-1", groupId: 1 },
+            { id: 2, title: "todo-2", groupId: 1 },
+            { id: 3, title: "todo-3", groupId: 1 },
+
+
+
+          ]
+        },
+        {
+          id: 2,
+          name: "In progress",
+          items: [
+            { id: 4, title: "todo-4", groupId: 2 },
+            { id: 5, title: "todo-5", groupId: 2 },
+            { id: 6, title: "todo-6", groupId: 2 },
+          ]
+
+
+        },
+        {
+          id: 3,
+          name: "Done",
+          items: [
+            { id: 7, title: "todo-7", groupId: 3 },
+            { id: 8, title: "todo-8", groupId: 3 },
+            { id: 9, title: "todo-9", groupId: 3 },
+          ]
+
+
+        }
+      ],
+
+      options: {
+        dropzoneSelector: ".drag-list",
+        draggableSelector: ".item-list"
+      }
+
+    }
+  },
+  methods: {
+    onGroupsChange(e) {
+      console.log({ e });
+    }
   }
+
+
 }
 </script>
 
-<style>
+<style scoped>
+.title {
+  margin-bottom: 20px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  max-width: 1100px;
+  margin: auto;
+  padding: 10px;
+}
+
+li {
+  padding: 20px 10px;
+  border: 1px solid rgb(207, 207, 207);
+  margin-bottom: 10px;
+}
+
+.group_name {
+  margin-bottom: 15px;
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueDraggable from 'vue-draggable'
+import { VueDraggableDirective } from 'vue-draggable'
 
+Vue.use(VueDraggable)
 Vue.config.productionTip = false
+
+export default {
+  directives: {
+    dragAndDrop: VueDraggableDirective
+  }
+}
 
 new Vue({
   render: h => h(App),
